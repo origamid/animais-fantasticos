3d70e9ecde67ba08ffef8f5a58a860493898cecc
Andre Rafael <andre@origamid.com>
2018-09-05 19:15:02 +0100

    acertado o caminho da api

--- js/script.js
+++ js/script.js
@@ -37,6 +37,6 @@ menuMobile.init();
 const funcionamento = new Functionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
